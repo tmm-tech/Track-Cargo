4231218fef9e916b2dc9a70691a5e3dc4c0e5e57
tmmtech <tonymugi074@gmail.com>
2025-05-13 23:52:12 +0300

    Track

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '@/views/HomePage.vue';
 import Track from '@/views/TrackPage.vue';
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/track', name: 'Track', component: Track },
+  { path: '/', name: 'Track', component: Home },
 ];
 
 const router = createRouter({
