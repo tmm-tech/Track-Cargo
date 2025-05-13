a8bbad6b084d0305ed662bde80bcd4d9e028b043
tmmtech <tonymugi074@gmail.com>
2025-05-14 00:38:16 +0300

    Track

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-  ],
-})
+    vue(),
+  
+  ]
+});
