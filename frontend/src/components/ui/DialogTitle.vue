887e09c866c3e297b3e6ca95086b5ead906e2b17
tmmtech <tonymugi074@gmail.com>
2025-05-16 22:55:26 +0300

    Frontend

--- frontend/src/components/ui/DialogTitle.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h2 class="text-lg font-semibold leading-none tracking-tight">
-    <slot></slot>
-  </h2>
-</template>
--- frontend/src/components/ui/TabsList.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground">
-    <slot></slot>
-  </div>
-</template>
