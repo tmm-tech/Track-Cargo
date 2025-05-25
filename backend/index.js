8846f0fd94bc2b833a8e6d6fef90ec3f3ef0dc8e
tmmtech <tonymugi074@gmail.com>
2025-05-25 22:00:37 +0300

    Backend

--- backend/index.js
+++ backend/index.js
@@ -4,28 +4,17 @@ const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const cloudinary = require('cloudinary').v2;
 const UserRoutes = require('./routes/UserRoutes');
-const ImageRoutes = require('./routes/ImageRoutes');
-const ProgramRoutes = require('./routes/ProgramRoutes');
-const NewsRoutes = require('./routes/NewsRoutes');
-const CareerRoutes = require('./routes/CareerRoutes');
-const EmployeeRoutes = require('./routes/EmployeeRoutes');
-const NotificationRoutes = require('./routes/NotificationRoutes'); 
+const PackageRoutes = require('./routes/PackageRoutes');
+const ActivityRoutes = require('./routes/ActivityRoutes'); 
 const cookieParser = require('cookie-parser');
 
 const app = express(); 
 
-// Middleware setup
-app.use(cookieParser());
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
+
 const allowedOrigins = [
-    'https://ketrb-cms-one.vercel.app',
-    'https://ketrb.netlify.app'
+    // 'https://ketrb-cms-one.vercel.app',
+    // 'https://ketrb.netlify.app'
   ];
   
   const corsOptions = {
@@ -60,29 +49,13 @@ app.use(addTokenToRequest);
 
 // Route handling
 app.use('/users', UserRoutes);
-app.use('/images', ImageRoutes);
-app.use('/programs', ProgramRoutes);
-app.use('/notifications', NotificationRoutes);
-app.use('/employees', EmployeeRoutes);
-app.use('/careers', CareerRoutes);
-app.use('/news', NewsRoutes);
+app.use('/package', PackageRoutes);
+app.use('/activity', ActivityRoutes);
+a
 app.get('/', (req, res) => {
-    res.json({ message: "Confirmed Connection to KETRB CMS" });
+    res.json({ message: "Confirmed Connection to Texmon LOGISTICS" });
 });
 
-app.get('/check-file/:folder/:filename', (req, res) => {
-    const { folder, filename } = req.params;
-    const filePath = path.join(__dirname, 'public', folder, filename);
-
-    fs.access(filePath, fs.constants.F_OK, (err) => {
-        if (err) {
-            // File does not exist
-            return res.status(404).json({ message: 'File not found' });
-        }
-        // File exists
-        return res.status(200).json({ message: 'File exists', filePath });
-    });
-});
 
 // Server setup
 const port = process.env.PORT || 4080;
