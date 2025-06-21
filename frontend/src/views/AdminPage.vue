27de96f79309493388f94b9006230f9feec91a4e
Tony Mwangi Mugi <tonymugi074@gmail.com>
2025-06-21 18:59:03 +0300

    Update AdminPage.vue

--- frontend/src/views/AdminPage.vue
+++ frontend/src/views/AdminPage.vue
@@ -433,10 +433,10 @@
                           <div class="flex items-center">
                             <div
                               class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-200 flex items-center justify-center text-[#273272] font-medium">
-                              {{ getInitials(user.fullname) }}
+                              {{ getInitials(user.fullname || 'User') }}
                             </div>
                             <div class="ml-4">
-                              <div class="text-sm font-medium text-gray-900">{{ user.fullname }}</div>
+                              <div class="text-sm font-medium text-gray-900">{{ user.fullname  || 'User'}}</div>
                               <div class="text-sm text-gray-500">{{ user.username }}</div>
                             </div>
                           </div>
