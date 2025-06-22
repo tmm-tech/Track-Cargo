54727bc092ef543cb3362a73ac295aa91009439d
Tony Mwangi Mugi <tonymugi074@gmail.com>
2025-06-22 11:52:45 +0300

    Update TrackPage.vue

--- frontend/src/views/TrackPage.vue
+++ frontend/src/views/TrackPage.vue
@@ -319,7 +319,7 @@ export default {
            // Make the real API call to fetch tracking info
           const response = await ShippingServices.trackPackage(trackingNumber.value);
       
-          if (response.data && response.data.success && response.data.data) {
+          if (response.data && response.data.success && response.data.package) {
             // Save to recent searches
             saveRecentSearch(trackingNumber.value);
       
